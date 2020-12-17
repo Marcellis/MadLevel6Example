c6e192564bb50b47c08a882a9be8f1076d277df5
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-12-17 17:38:57 +0100

    converted to viewbinding

--- app/src/main/java/nl/hva/level6example/ui/MainActivity.kt
+++ app/src/main/java/nl/hva/level6example/ui/MainActivity.kt
@@ -5,21 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.activity_main.*
 import nl.hva.level6example.R
+import nl.hva.level6example.databinding.ActivityMainBinding
 import nl.hva.level6example.vm.TriviaViewModel
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: TriviaViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setSupportActionBar(binding.toolbar)
 
-        fab.setOnClickListener {
+        binding.fab.setOnClickListener {
             viewModel.getTriviaNumber()
         }
     }
